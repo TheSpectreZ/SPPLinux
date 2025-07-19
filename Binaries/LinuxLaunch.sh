5f0c3bd39eef5b3732085c01083c689e841da8b4
TheSpectreZ <ishanchaudhari7203m@gmail.com>
2025-07-19 09:48:27 +0530

    Binaries

---
+++ Binaries/LinuxLaunch.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+NAME=$1
+PORT=$2
+
+# Get the directory of the current script
+BINARY_PATH=$(dirname "$(readlink -f "$0")")
+
+cd "$BINARY_PATH"
+
+# Define target directories
+TARGETS=(
+    "../3rdParty/ozz-animation/lib"
+    "../3rdParty/jsoncpp/lib"
+    "../3rdParty/easy_profiler/lib"
+    "../3rdParty/Physx5/bin/linux.x86_64/release"
+    "../3rdPartyPrivate/SteamWorks/redistributable_bin/linux64"
+)
+
+# Construct LD_LIBRARY_PATH
+for TARGET in "${TARGETS[@]}"; do
+    ABSOLUTE_PATH=$(realpath "$BINARY_PATH/$TARGET")
+    LD_LIBRARY_PATH="$ABSOLUTE_PATH:$LD_LIBRARY_PATH"
+done
+
+# Export updated LD_LIBRARY_PATH
+export LD_LIBRARY_PATH
+
+./"$NAME" -port=${PORT}  -nogc
